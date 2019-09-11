e675af0acd1e6fa6e26bb7cd458cc27fd8877fb6
unknown <bunnyppl@hotmail.com>
2019-09-11 22:29:09 +0800

    fix

--- test/new/appnew.test.js
+++ test/new/appnew.test.js
@@ -1,12 +1,12 @@
 describe('app', () => {
     beforeEach(async () => {
-      jest.setTimeout(30000);
+      
       await page.goto("http://localhost:3000/")
     })
   
     it('should display a PopQuiz heading', async () => {
       await page.screenshot({path: 'test/screenshots/mainpage.png'});
-      await page.waitForSelector('PopQuiz')
+      await expect(page).toMatch('PopQuiz')
     })
 
     it('click manage pop quiz ', async () => {
@@ -16,7 +16,7 @@ describe('app', () => {
     it('should display first record', async () => {
       await page.goto("http://localhost:3000/list-quizes")
       await page.screenshot({path: 'test/screenshots/listofquizes.png'});
-      await page.waitForSelector('What colour is Donald Trump\'s hair\?')
+      await expect(page).toMatch('What colour is Donald Trump\'s hair\?')
     })
    
     it('add a new pop quiz', async () => {
@@ -59,7 +59,7 @@ describe('app', () => {
     it('list all pop quizes after adding', async () => {
       await page.goto("http://localhost:3000/list-quizes")
       await page.screenshot({path: 'test/screenshots/newquiz.png'});
-      await page.waitForSelector('What is your name')
+      await expect(page).toMatch('What is your name')
     })
 
   
