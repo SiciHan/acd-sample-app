01229af157c32846dcf7e4ed97c33ad5bd1c075c
Kenneth Phang <bunnyppl@gmail.com>
2019-09-12 08:42:10 +0800

    remove test dir

--- test/current/appcurrent.test.js
+++
@@ -1,9 +0,0 @@
-describe('app', () => {
-    beforeEach(async () => {
-      await page.goto('http://localhost:3000/')
-    })
-  
-    it('should display a PopQuiz heading', async () => {
-      await expect(page).toMatch('PopQuiz')
-    })
-  })
